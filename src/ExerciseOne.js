f03340c3c6dbb0928d175d3a484c125147d53bda
Juan Manuel Fernandez Daza <tachitofer@hotmail.com>
2020-02-03 17:59:12 -0500

    This is the first exercise

---
+++ src/ExerciseOne.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+	var number = 0;
+
+  return (
+    <div className="App">
+	 
+	 function(number){
+
+		if(number == 0){
+
+			console.log(`This number is: ${number}`);
+
+		}else if(number > 0){
+
+			console.log(`This number is: ${number}. It is a positive number`);
+
+		}else{
+
+			console.log(`This number is: ${number}. It is a negative number`);
+
+	}
+}
+
+    </div>
+  );
+}
+
+export default App;
+
